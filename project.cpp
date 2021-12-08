e8f50cffbfb08dd75477f03eef514b509ee03076
mohammadmolavi <mohammadmolavi97@gmail.com>
2021-12-09 00:42:23 +0330

    stack implemented

---
+++ project.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+class node
+{
+public:
+    T data;
+    node *next;
+};
+
+template <typename T>
+class linkedList
+{
+public:
+    node<T> *head;
+    linkedList() { head = NULL; }
+    void insertElement(T data)
+    {
+        node<T> *temp = new node<T>;
+        temp->data = data;
+        temp->next = NULL;
+        if (head == NULL)
+            head = temp;
+        else
+        {
+            temp->next = head;
+            head = temp;
+        }
+    }
+    void deleteElement() { head = head->next; }
+    void print()
+    {
+        node<T> *cur = new node<T>;
+        cur = head;
+        while (cur != NULL)
+        {
+            cout << cur->data << " ";
+            cur = cur->next;
+        }
+        cout << endl;
+    }
+};
+
+template <typename T>
+class stack1
+{
+private:
+    linkedList<T> s;
+    int sz;
+
+public:
+    stack1<T>()
+    {
+        sz = 0;
+    }
+    bool empty() { return size() == 0; }
+    T pop()
+    {
+        if (!empty())
+        {
+            T data = s.head->data;
+            s.deleteElement();
+            sz--;
+            return data;
+        }
+        return NULL;
+    }
+    void push(T data) { s.insertElement(data), sz++; }
+    int size() { return sz; }
+    T top()
+    {
+        if (empty())
+            return NULL;
+        return s.head->data;
+    }
+};
